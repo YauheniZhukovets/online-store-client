013a1e167a71dbd0bf816edb36d2bca2e007b916
zhukavets <stewart-8@mail.ru>
2022-11-20 22:41:26 +0300

    added logic in admin panel

--- src/page/Admin.jsx
+++ src/page/Admin.jsx
@@ -1,17 +1,40 @@
 import {Button, Container} from "react-bootstrap";
+import {CreateBrand} from "../components/modals/CreateBrand";
+import {CreateDevice} from "../components/modals/CreateDevice";
+import {CreateType} from "../components/modals/CreateType";
+import {useState} from "react";
 
 export const Admin = () => {
+    const [brandVisible, setBrandVisible] = useState(false);
+    const [typeVisible, setTypeVisible] = useState(false);
+    const [deviceVisible, setDeviceVisible] = useState(false);
+
     return (
         <Container className="d-flex flex-column">
-            <Button variant={'outline-dark'} className='mt-4 p-2'>
+            <Button onClick={()=> setTypeVisible(true)}
+                variant={'outline-dark'} className='mt-4 p-2'>
                 Добавить тип
             </Button>
-            <Button variant={'outline-dark'} className='mt-4 p-2'>
+            <Button onClick={()=> setBrandVisible(true)}
+                variant={'outline-dark'} className='mt-4 p-2'>
                 Добавить бренд
             </Button>
-            <Button variant={'outline-dark'} className='mt-4 p-2'>
+            <Button onClick={()=> setDeviceVisible(true)}
+                variant={'outline-dark'} className='mt-4 p-2'>
                 Добавить устройство
             </Button>
+            <CreateBrand show={brandVisible}
+                         onHide={() => setBrandVisible(false)}
+            >
+            </CreateBrand>
+            <CreateDevice show={deviceVisible}
+                          onHide={() => setDeviceVisible(false)}
+            >
+            </CreateDevice>
+            <CreateType show={typeVisible}
+                        onHide={() => setTypeVisible(false)}
+            >
+            </CreateType>
         </Container>
     )
 }
